1cbd007c07cff9f3a44ce0229304d4ff96daf4e2
Lucas Sandoval <sandovalucas88@gmail.com>
2021-12-07 17:48:38 -0300

    Se crearon los formularios reactivos 'login' y 'registro'

--- src/app/view/login/login.component.ts
+++ src/app/view/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formLogin:FormGroup;
+  router:Router;
+
+  constructor(router: Router) {
+    this.router = router;
+    this.formLogin = new FormGroup({
+      email: new FormControl('', Validators.required),
+      contraseña: new FormControl('', Validators.required)
+    });
+  }
 
   ngOnInit(): void {
+
+  }
+
+  registro(){
+    this.router.navigate(['/registro']);
   }
 
 }
--- src/app/view/registro/registro.component.ts
+++ src/app/view/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  formRegistro:FormGroup;
+  router:Router;
+
+  constructor(router:Router) {
+    this.router = router;
+    this.formRegistro = new FormGroup({
+      username: new FormControl(''),
+      email: new FormControl(''),
+      newcontraseña: new FormControl(''),
+      againcontraseña: new FormControl('')
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  login() {
+    this.router.navigate(['/login']);
   }
 
 }
