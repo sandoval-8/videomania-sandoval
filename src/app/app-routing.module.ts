3d979bb13ba778e388ec514634381bd00d03e7a4
Lucas Sandoval <sandovalucas88@gmail.com>
2021-12-07 18:38:23 -0300

    Agregamos la carpeta de imagenes de peliculas para los ejemplos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {FormComponent} from "../../../videoclub-sandoval/src/app/components/form/form.component";
-import {LoginComponent} from "../../../videoclub-sandoval/src/app/view/login/login.component";
-import {RegistroComponent} from "../../../videoclub-sandoval/src/app/view/registro/registro.component";
-import {ListadoComponent} from "../../../videoclub-sandoval/src/app/components/listado/listado.component";
-import {InfoComponent} from "../../../videoclub-sandoval/src/app/components/info/info.component";
-import {CarritoComponent} from "../../../videoclub-sandoval/src/app/components/carrito/carrito.component";
+
+import {FormComponent} from "./components/form/form.component";
+import {LoginComponent} from "./view/login/login.component";
+import {RegistroComponent} from "./view/registro/registro.component";
+import {ListadoComponent} from "./components/listado/listado.component";
+import {InfoComponent} from "./components/info/info.component";
+import {CarritoComponent} from "./components/carrito/carrito.component";
 
 const routes: Routes = [
   { path: 'form', component:FormComponent },
