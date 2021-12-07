997f2ea23bc74120b8bb3f267ac306b39514d7e7
Lucas Sandoval <sandovalucas88@gmail.com>
2021-12-07 16:56:59 -0300

    creamos la carpeta 'components' y dentro creamos los componentes 'carrito', 'form', 'info', listado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './view/login/login.component';
 import { RegistroComponent } from './view/registro/registro.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
+import { InfoComponent } from './components/info/info.component';
+import { ListadoComponent } from './components/listado/listado.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegistroComponent
+    RegistroComponent,
+    CarritoComponent,
+    InfoComponent,
+    ListadoComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
