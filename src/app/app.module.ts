2677d0bdc80efd95fee24609aa818e70fded1b7a
Lucas Sandoval <sandovalucas88@gmail.com>
2021-12-07 16:47:00 -0300

    Se creo la carpeta 'view' que va a tener los componentes 'login' y 'registro'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './view/login/login.component';
+import { RegistroComponent } from './view/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
